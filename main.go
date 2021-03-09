bda790639188f795bbf54222518413923657476b
Confiks <confiks@scriptbase.org>
2021-03-09 16:08:00 +0100

    refactor: Switch from stdin and stdout to intput and output files

--- main.go
+++ main.go
@@ -1,40 +1,53 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
+	var inputFilePath string
+	var outputFilePath string
 	var isEncode bool
 	var isDecode bool
 
+	flag.StringVar(&inputFilePath, "i", "", "Input file to encode or decode")
+	flag.StringVar(&outputFilePath, "o", "", "Output file to write encoding or decoding result to")
 	flag.BoolVar(&isEncode, "e", true, "Encode QR encoded string (default)")
 	flag.BoolVar(&isDecode,"d", false, "Decode QR encoded string")
 	flag.Parse()
 
-	var stdinBuffer bytes.Buffer
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		stdinBuffer.Write(scanner.Bytes())
+	if inputFilePath == "" || outputFilePath == "" {
+		exit("No input or output file path provided")
 	}
 
-	var bytesStdout []byte
+	if _, err := os.Stat(inputFilePath); os.IsNotExist(err) {
+		exit("Input file does not exist")
+	}
+
+	inputBytes, err := ioutil.ReadFile(inputFilePath)
+	if err != nil {
+		exit("File could not be read: " + err.Error())
+	}
+
+	var outputBytes []byte
 	if isDecode {
 		var err error
-		bytesStdout, err = qrDecode(stdinBuffer.Bytes())
+		outputBytes, err = qrDecode(inputBytes)
 
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error decoding QR base45 string. Did you forget to escape dollar signs or used single quotes to echo?")
-			os.Exit(1)
+			exit("Error decoding QR base45 string. Did you forget to escape dollar signs or used single quotes to echo?")
 		}
 	} else {
-		bytesStdout = qrEncode(stdinBuffer.Bytes())
+		outputBytes = qrEncode(inputBytes)
 	}
 
-	fmt.Println(string(bytesStdout))
+	ioutil.WriteFile(outputFilePath, outputBytes, 0644)
+}
+
+func exit(errorMsg string) {
+	fmt.Fprintln(os.Stderr, errorMsg)
+	os.Exit(1)
 }
