4a71e250198830757d6fb60b39105bdab4740403
Confiks <confiks@scriptbase.org>
2021-03-09 12:35:52 +0100

    feat: Add decoding mode, better stdin reading

--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	bytesStdin, _ := reader.ReadBytes('\n')
+	var isEncode bool
+	var isDecode bool
 
-	qrEncodedStdin := qrEncode(bytesStdin)
-	fmt.Println(string(qrEncodedStdin))
+	flag.BoolVar(&isEncode, "e", true, "Encode QR encoded string (default)")
+	flag.BoolVar(&isDecode,"d", false, "Decode QR encoded string")
+	flag.Parse()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	bytesStdin := scanner.Bytes()
+
+	var bytesStdout []byte
+	if isDecode {
+		var err error
+		bytesStdout, err = qrDecode(bytesStdin)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error decoding QR base45 string: "+err.Error())
+			os.Exit(1)
+		}
+	} else {
+		bytesStdout = qrEncode(bytesStdin)
+	}
+
+	fmt.Println(string(bytesStdout))
 }
